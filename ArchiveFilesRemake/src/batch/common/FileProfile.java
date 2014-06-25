4ec61e506f5852e93f0f55382956d8d536370346
admin <admin@t100ta>
2014-06-26 01:12:12 +0900

    Revert "Revert "ディレクトリ作成処理追加 プロファイル作成処理追加 ファイルコピー処理実装中""
    This reverts commit 417f0d6820ff0555c80660124b608487ab2cea54.

---
+++ ArchiveFilesRemake/src/batch/common/FileProfile.java
@@ -0,0 +1,64 @@
+package batch.common;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class FileProfile {
+	private final String CSV_FILENAME = "profile.csv";
+	/* 取得時ファイル名 */
+	private String defaultFileName;
+	/* 取得日時 */
+	private String addDate;
+	/* 再編後ファイル名 */
+	private String remakeFileName;
+
+	public FileProfile(String defaultFileName, String addDate) {
+		this.defaultFileName = defaultFileName;
+		this.addDate = addDate;
+	}
+
+	public static String getLastModifiedString(long lastModified) {
+		String lastModifiedString = "";
+
+		/** 時刻フォーマット変換 */
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.JAPAN);
+		lastModifiedString = sdf.format(new Long(lastModified));
+
+		return lastModifiedString;
+	}
+
+	public void writeCsvFile(String path) {
+		try {
+			File file = new File(path + "/" + CSV_FILENAME);
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+			pw.println(this.defaultFileName + "," + this.addDate);
+			pw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	public String getDefaultFileName() {
+		return defaultFileName;
+	}
+	public void setDefaultFileName(String defaultFileName) {
+		this.defaultFileName = defaultFileName;
+	}
+	public String getRemakeFileName() {
+		return remakeFileName;
+	}
+	public void setRemakeFileName(String remakeFileName) {
+		this.remakeFileName = remakeFileName;
+	}
+	public String getAddDate() {
+		return addDate;
+	}
+	public void setAddDate(String addDate) {
+		this.addDate = addDate;
+	}
+
+}
--- ArchiveFilesRemake/src/batch/common/constant.java
+++ ArchiveFilesRemake/src/batch/common/constant.java
@@ -3,7 +3,7 @@ package batch.common;
 public class constant {
 
 	/** 検索ディレクトリ */
-	public static final String SERCH_DIR = "C:/Users/admin_2/Downloads/";
+	public static final String SERCH_DIR = "C:/work/picture/";
 
 	/** 移動先ディレクトリ */
 	public static final String TARGET_DIR = "D:/data/ArchiveFile/";
