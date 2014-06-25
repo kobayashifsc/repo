417f0d6820ff0555c80660124b608487ab2cea54
admin <admin@t100ta>
2014-06-26 01:10:46 +0900

    Revert "ディレクトリ作成処理追加 プロファイル作成処理追加 ファイルコピー処理実装中"
    This reverts commit 732f20c2afef040509dd41d9ef4fd360907949da.

--- ArchiveFilesRemake/src/batch/common/constant.java
+++ ArchiveFilesRemake/src/batch/common/constant.java
@@ -3,7 +3,7 @@ package batch.common;
 public class constant {
 
 	/** 検索ディレクトリ */
-	public static final String SERCH_DIR = "C:/work/picture/";
+	public static final String SERCH_DIR = "C:/Users/admin_2/Downloads/";
 
 	/** 移動先ディレクトリ */
 	public static final String TARGET_DIR = "D:/data/ArchiveFile/";
--- ArchiveFilesRemake/src/batch/main/ArchiveFileSelection.java
+++ ArchiveFilesRemake/src/batch/main/ArchiveFileSelection.java
@@ -1,10 +1,7 @@
 package batch.main;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 
-import batch.common.FileProfile;
 import batch.common.constant;
 
 public class ArchiveFileSelection {
@@ -18,64 +15,34 @@ public class ArchiveFileSelection {
 
 		// ディレクトリを読み込む
 		File file = new File(inpath);
-		File files[] = file.listFiles();
+        File files[] = file.listFiles();
 
 		// 対象ファイルが存在する間繰り返す
-		for (int i = 0; i < files.length; i++) {
-			if (isArchiveFile(files[i].getName())) {
-				// System.out.println("ファイル" + (i + 1) + "→" +
-				// files[i].getName());
-
-				// 移動先のディレクトリ作成
-				String subdir = removeFileExtension(files[i].getName());
-				File outdir = new File(outpath + subdir);
-				//System.out.println("ファイル" + (i + 1) + "→" + outdir.getPath());
-				outdir.mkdir();
-
-				// Profile作成
-				String lastModified = FileProfile
-						.getLastModifiedString(files[i].lastModified());
-				FileProfile fp = new FileProfile(files[i].getName(),
-						lastModified);
-				fp.writeCsvFile(outdir.getPath());
-				// ファイルコピー TODO
-				try {
-					System.out.println(files[i].toPath().toString());
-					System.out.println(outdir.toPath().toString());
-					Files.copy(files[i].toPath(), outdir.toPath(),
-							StandardCopyOption.COPY_ATTRIBUTES);
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				// コピー元削除
-				// 保留
-			}
-		}
+        for (int i = 0; i < files.length; i++) {
+        	if (isArchiveFile( files[i].getName())){
+            	// ファイル名取得
+        		// System.out.println("ファイル" + (i + 1) + "→" + filename);
+        		// 移動先のディレクトリ作成
+        		String subdir ="";
+        		// TODO
+        		File outdir = new File(outpath + subdir);
+        		// Profile作成
+        		// TODO
+        		// ファイル移動
+        		// TODO
+        	}
+        }
 	}
 
 	// アーカイブファイル判定
 	private boolean isArchiveFile(String filename) {
-		final String[] IMAGE_EXTENSION_ARRAY = { ".rar" };
+		final String[] IMAGE_EXTENSION_ARRAY = {".rar"};
 		// ファイルの末尾がアーカイブ拡張子リストの中にあるかチェック
-		for (int i = 0; i < IMAGE_EXTENSION_ARRAY.length; i++) {
+		for (int i = 0;i < IMAGE_EXTENSION_ARRAY.length;i++ ) {
 			if (filename.toLowerCase().endsWith(IMAGE_EXTENSION_ARRAY[i])) {
 				return true;
 			}
 		}
 		return false;
-	}
-
-	// 拡張子削除
-	public static String removeFileExtension(String filename) {
-		int lastDotPos = filename.lastIndexOf('.');
-
-		if (lastDotPos == -1) {
-			return filename;
-		} else if (lastDotPos == 0) {
-			return filename;
-		} else {
-			return filename.substring(0, lastDotPos);
-		}
 	}
 }
